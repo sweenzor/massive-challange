7e4cd6f88dcef22b1f90a023934b4eb45cfc63ae
Matt Sweeney <mattswe@gmail.com>
2011-09-07 22:12:58 -0700

    War is recursive, but doesn't call battle (that introduced some unnecessary complication)

--- simulate-war.py
+++ simulate-war.py
@@ -16,37 +16,46 @@ def create_deck():
 
 	return player1, player2
 
-def battle(player1, player2, ante):
+def battle(player1, player2):
 	# "Each player draws a card, higher value card wins both"
 	if player1[0] > player2[0]:
-		player1.append(ante+player1[0]+player2[0])
+		player1.extend([player1[0],player2[0]])
 		del player1[0], player2[0]
 
 	if player1[0] < player2[0]:
-		player2.append(ante+player2[0]+player1[0])
+		player2.extend([player2[0],player1[0]])
 		del player1[0], player2[0]
 
 	# "In the even of a tie, play a war"
 	if player1[0] == player2[0]:
-		ante.append(player1[0])
-		ante.append(player2[0])
+		ante = []
+		ante.extend([player1[0],player2[0]])
+		del player1[0], player2[0]
 		war(player1, player2, ante)
-
 
 def war(player1, player2, ante):
 	# "Each player antes three cards, then plays another"
-	player1_draw = player1[1:4]
-	player2_draw = player2[1:4]
+	player1_draw = random.choice(player1[0:3])
+	player2_draw = random.choice(player2[0:3])
 
-	battle(random.choice(player1[1:4]), random.choice(player1[1:4]))
+	if player1_draw > player2_draw:
+		player1.extend(ante+player1[0:3]+player2[0:3])
+		del player1[0:3], player2[0:3]
 
-	return cards
+	if player1_draw < player2_draw:
+		player2.extend(ante+player2[0:3]+player1[0:3])
+		del player1[0:3], player2[0:3]
+	
+	if player1_draw == player2_draw:
+		ante.extend(player2[0:3]+player1[0:3])
+		del player1[0:3], player2[0:3]
+		war(player1, player2, ante)
 
 
 
 player1, player2 = create_deck()
 ante = []
 while (len(player1) > 0) & (len(player2) > 0):
-	battle(player1,player2,ante)
-	print player1
-	print player2
+	battle(player1,player2)
+	print 'player1: ',player1
+	print 'player2: ',player2, '\n'
