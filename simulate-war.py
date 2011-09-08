5a0ab41ef3cc123344c91bc7426a5756f4d21f4e
Matt Sweeney <mattswe@gmail.com>
2011-09-07 19:45:51 -0700

    Restructure into functions

--- simulate-war.py
+++ simulate-war.py
@@ -1,19 +1,22 @@
 import random
 
-# create deck of cards
-# no suits just face values:
-# A K Q J 10 9 8 7 6 5 4 3 2
-deck = range(2,15)*4
+def create_deck():
+	# create deck of cards
+	# no suits just face values:
+	# A K Q J 10 9 8 7 6 5 4 3 2
+	deck = range(2,15)*4
 
-# shuffle
-random.shuffle(deck)
+	# shuffle
+	random.shuffle(deck)
 
-#cut deck
-cut_size = len(deck)/2
-player1 = deck[:cut_size]
-player2 = deck[cut_size:]
+	#cut deck
+	cut_size = len(deck)/2
+	player1 = deck[:cut_size]
+	player2 = deck[cut_size:]
 
-while (len(player1) > 0) & (len(player2) > 0):
+	return player1, player2
+
+def battle(player1, player2):
 	if player1[0] > player2[0]:
 		player1.append(player1[0])
 		player1.append(player2[0])
@@ -25,8 +28,19 @@ while (len(player1) > 0) & (len(player2) > 0):
 		del player1[0], player2[0]
 
 	if player1[0] == player2[0]:
-		print random.choice(player1[1:4])
-		print random.choice(player2[1:4])
+		war(player1, player2)
+
+
+def war(player1, player2):
+	# Each player antes three cards, then plays another card
+	print random.choice(player1[1:4])
+	print random.choice(player2[1:4])
+	return cards
+
+
+
+player1, player2 = create_deck()
+while (len(player1) > 0) & (len(player2) > 0):
 
 	print player1
 	print player2
