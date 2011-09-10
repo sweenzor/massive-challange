ba1f3cf268867ad09093a31884a069b4a986637e
Matt Sweeney <mattswe@gmail.com>
2011-09-10 04:20:23 -0700

    Create basic class outline

--- simulate-war.py
+++ simulate-war.py
@@ -5,3 +5,33 @@ import time
 from collections import Counter
 
 
+class WarGame(object):
+
+	def battle(self):
+		pass
+	def war(self):
+		pass
+
+class Player(object):
+
+	def draw(self):
+		pass
+
+class Deck(object):
+	
+	def create(self):
+		pass
+	def deal(self):
+		pass
+
+
+class Card(object):
+	"""Bare bones card class."""
+	def __init__(self, value):
+		self.value = value
+		return None
+
+
+if __name__ == "__main__":
+	c = Card(1)
+	print c.value
