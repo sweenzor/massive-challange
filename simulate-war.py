446590e646e76ce4d2691abce3323241357bc531
Matt Sweeney <mattswe@gmail.com>
2011-09-10 04:43:10 -0700

    Create deck class

--- simulate-war.py
+++ simulate-war.py
@@ -19,19 +19,39 @@ class Player(object):
 
 class Deck(object):
 	
-	def create(self):
-		pass
-	def deal(self):
-		pass
+	def __init__(self):
+		"""Create deck of cards, no suits just face values:
+		A K Q J 10 9 8 7 6 5 4 3 2"""
+		
+		self.deck = []
+		for card_value in range(2,15)*4:
+			self.deck.append(Card(card_value))
 
+		# shuffle (assuming that pseudo random is good enough)
+		random.shuffle(self.deck)
+
+		return None
+
+	def deal(self):
+		# cut the deck
+		cut_size = len(self.deck)/2
+		sub_deck1 = self.deck[:cut_size]
+		sub_deck2 = self.deck[cut_size:]
+
+		# return the halved decks
+		return sub_deck1, sub_deck2
 
 class Card(object):
 	"""Bare bones card class."""
 	def __init__(self, value):
 		self.value = value
 		return None
+	
+	def __str__(self):
+		return self.value
 
 
 if __name__ == "__main__":
-	c = Card(1)
-	print c.value
+	d = Deck()
+	a,b = d.deal()
+	print len(a), len(b)
